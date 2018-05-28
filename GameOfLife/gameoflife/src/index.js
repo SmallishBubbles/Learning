ea7b5776d5bcaaed1310c6b13e2df59c7863fb39
MarishaHoza <marisha.hoza@gmail.com>
2018-05-28 13:16:56 -0700

    finished tutorial with new styles

---
+++ GameOfLife/gameoflife/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Main from './main.jsx'
+
+// react game of life tutorial by freeCodeCamp
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
+
+
---
+++ GameOfLife/gameoflife/src/main.jsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import Buttons from './Buttons.jsx';
+import Grid from './grid.jsx';
+
+
+function arrayClone(arr) {
+    return JSON.parse(JSON.stringify(arr));
+}
+
+
+class Main extends React.Component {
+    constructor() {
+        super();
+
+        this.speed = 100;
+        this.rows = 30;
+        this.cols = 50;
+
+        this.state = {
+            generation: 0,
+            gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false)),
+        };
+    }
+
+    selectBox = (row, col) => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        gridCopy[row][col] = !gridCopy[row][col];
+        this.setState({
+            gridFull: gridCopy,
+        });
+    };
+
+    seed = () => {
+        let gridCopy = arrayClone(this.state.gridFull);
+        for (var i = 0; i < this.rows; i++) {
+            for (var j = 0; j < this.cols; j++) {
+                if (Math.floor(Math.random() * 4) === 1) {
+                    gridCopy[i][j] = true;
+                }
+            }
+        }
+        this.setState({
+            gridFull: gridCopy,
+        });
+    };
+
+    playButton = () => {
+        clearInterval(this.intervalId);
+        this.intervalId = setInterval(this.play, this.speed);
+    };
+
+    pauseButton = () => {
+        clearInterval(this.intervalId);
+    };
+
+    fast = () => {
+        this.speed = 100;
+        this.playButton();
+    };
+
+    slow = () => {
+        this.speed = 1000;
+        this.playButton();
+    };
+
+    clear = () => {
+        let grid = Array(this.rows).fill().map(() => Array(this.cols).fill(false));
+        clearInterval(this.intervalId);
+        this.setState({
+            gridFull: grid,
+            generation: 0
+        });
+    };
+
+    gridSize = (size) => {
+        switch (size) {
+            case "1":
+                this.cols = 20;
+                this.rows = 10;
+                break;
+            case "2":
+                this.cols = 50;
+                this.rows = 30;
+                break;
+            default:
+                this.cols = 70;
+                this.rows = 50;
+        }
+        this.clear();
+    }
+
+
+    play = () => {
+        let g = this.state.gridFull;
+        let g2 = arrayClone(this.state.gridFull);
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                let count = 0;
+
+                // these next 8 lines check for neighbours
+                if (i > 0) if (g[i - 1][j]) count++;
+                if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+                if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+                if (j < this.cols - 1) if (g[i][j + 1]) count++;
+                if (j > 0) if (g[i][j - 1]) count++;
+                if (i < this.rows - 1) if (g[i + 1][j]) count++;
+                if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+                if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+
+                if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+                if (!g[i][j] && count === 3) g2[i][j] = true;
+            }
+        }
+        this.setState({
+            gridFull: g2,
+            generation: this.state.generation + 1,
+        });
+    };
+
+    componentDidMount() {
+        this.seed();
+        this.playButton();
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> The Game of Life </h1>
+                <Buttons
+                    playButton={this.playButton}
+                    pauseButton={this.pauseButton}
+                    slow={this.slow}
+                    fast={this.fast}
+                    clear={this.clear}
+                    seed={this.seed}
+                    gridSize={this.gridSize}
+                />
+
+                <Grid gridFull={this.state.gridFull} rows={this.rows} cols={this.cols} selectBox={this.selectBox} />
+
+                <h2> Generations: {this.state.generation}</h2>
+            </div>
+        );
+    }
+}
+
+
+export default Main;
