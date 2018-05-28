69068c5ddf1e8c4fd1d7697da56431d96e522163
MarishaHoza <marisha.hoza@gmail.com>
2018-05-27 19:28:25 -0700

    git ignore update

--- Flashcard-app/flashcards/src/config/firebase/db_config.js
+++
@@ -1,8 +0,0 @@
-export const DB_CONFIG = {
-    apiKey: "AIzaSyClpXvRAnA2tnxa5hre3gA6_bQzUnqwrrM",
-    authDomain: "flashcard-app-84e6a.firebaseapp.com",
-    databaseURL: "https://flashcard-app-84e6a.firebaseio.com",
-    projectId: "flashcard-app-84e6a",
-    storageBucket: "flashcard-app-84e6a.appspot.com",
-    messagingSenderId: "468048604168"
-}
