72a323b5ec30b54d6fe4bcc297f66e83108b5295
MarishaHoza <marisha.hoza@gmail.com>
2018-10-08 15:43:01 -0700

    hash table info and example

--- CompSci/implementingDataStructures.js
+++ CompSci/implementingDataStructures.js
@@ -445,3 +445,50 @@ class Node {
         return ans;
     }
 }
+
+
+
+/*
+
+  A HashTableSet!
+  
+  Hash tables are a way to keep track of information and have a super fast lookup time. 
+  The quality of the hashing algorithm is super important so that there are no overlapping values.
+  There should also be a steady hash generation so that the same input produces the same result whenever it is called.
+  Hash tables do not keep track of order.
+
+
+  /// example basic hash table
+
+
+  With a set, you want to put in a value to check later if it's in the collection.
+  You are going to watch a sufficiently large array to assure you don't have collisions. I did 255
+  to start with. When added, use a hashing function to hash the string and put in your table.
+  The class should have three functions:
+  
+  add -   function - takes a string as an input, hashes it, and puts in its table
+  check - function - takes a string and returns true if it exists in its table; otherwise returns false
+  hash -  function - takes a string and a max number and return a number between 0 and the max number
+                     function must be idempotent; the same string and max number will always yield the
+                     same output        
+*/
+
+class HashTableSet {
+    constructor() {
+        this.table = new Array(255);
+    }
+    add(input) {
+        this.table[this.hash(input, 255)] = input;
+    }
+    check(input) {
+        return !!this.table[this.hash(input, 255)];
+    }
+    hash(input, max) {
+        let num = 0;
+        for (let i = 0; i < input.length; i++) {
+            num += input.charCodeAt(i) * i;
+        }
+        return num % max;
+    }
+}
+
