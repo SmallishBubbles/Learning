c95c36bab7dc70f20b3be46ffe590347bb812897
MarishaHoza <marisha.hoza@gmail.com>
2018-10-18 22:17:01 -0700

    breadth first tree traversal

--- CompSci/treeTraversal.js
+++ CompSci/treeTraversal.js
@@ -127,5 +127,96 @@ describe('tests', function () {
 
 
 
+// depth first is good for getting as far away from the root node as possible first
+
+
+
+// breadth first traversal is good when you want to stay as close to the root node as possible
+// process the tree as flat as possible.
+// uses a queue (first in, first out)
+
+
+// recursive
+const breadthFirstTraverse = (queue, array) => {
+    if (!queue.length) return array;
+    const node = queue.shift();
+    array.push(node.value);
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+    return breadthFirstTraverse(queue, array);
+}
+
+// iterative
+const breadthFirstTraverse2 = (queue, array) => { 
+    if (!queue.length) return array;  // root node is already passed in to the queue
+
+    while (queue.length) {
+        const node = queue.shift();
+        array.push(node.value);
+        if (node.left) queue.push(node.left);
+        if (node.right) queue.push(node.right);
+    }
+
+    return array;
+}
+
+
+// unit tests
+// do not modify the below code
+describe('tests', function () {
+    const answer = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
+
+    const tree = {
+        value: "A",
+        left: {
+            value: "B",
+            left: {
+                value: "D",
+                left: {
+                    value: "G",
+                    left: null,
+                    right: null
+                },
+                right: null
+            },
+            right: {
+                value: "E",
+                left: null,
+                right: {
+                    value: "H",
+                    left: {
+                        value: "K",
+                        left: null,
+                        right: null
+                    }
+                }
+            }
+        },
+        right: {
+            value: "C",
+            left: {
+                value: "F",
+                left: {
+                    value: "I",
+                    left: null,
+                    right: null
+                },
+                right: {
+                    value: "J",
+                    left: null,
+                    right: null
+                }
+            },
+            right: null
+        }
+    };
+
+    render(tree, answer);
+
+    it('breadthFirstTraverse', () => {
+        expect(breadthFirstTraverse([tree], [])).toEqual(answer);
+        expect(breadthFirstTraverse2([tree], [])).toEqual(answer);
+    });
+});
 
 
