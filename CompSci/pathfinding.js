e31b2bbcc0cdafedbb86240006052b77e16f6954
MarishaHoza <marisha.hoza@gmail.com>
2018-12-03 22:26:12 -0800

    basic pathfinding explanation

---
+++ CompSci/pathfinding.js
@@ -0,0 +1,8 @@
+// pathfinding
+// if there are no barriers, something simple like 'go towards ntil you hit'would work
+// however, if you add obsticles, this breaks down very fast.
+
+// Simplified version of Dijkstra's algorithm solves this
+// go one step around the start point, one step around the end point
+// one step further (around the points you just created)... etc
+// when the points intersect, you have found the shortest possible path
